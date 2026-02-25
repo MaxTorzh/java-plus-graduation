197be11c5aa794119b1bcbb2b9be77965f18a7f2
Maksim Torzhkov <maxtorj@yandex.ru>
2026-02-25 15:20:44 +0300

    feat: add service part

---
+++ core/main-service/src/main/java/ru/practicum/explorewithme/main/model/EventState.java
@@ -0,0 +1,21 @@
+package ru.practicum.explorewithme.main.model;
+
+/**
+ * Состояния жизненного цикла события
+ */
+public enum EventState {
+    /**
+     * Ожидает модерации
+     */
+    PENDING,
+
+    /**
+     * Опубликовано
+     */
+    PUBLISHED,
+
+    /**
+     * Отменено
+     */
+    CANCELED
+}
