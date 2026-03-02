8154f5b74429e20cf9cfcc997ca403d63d2179db
Maksim Torzhkov <maxtorj@yandex.ru>
2026-03-02 16:04:38 +0300

    feat: add kafka

---
+++ stats/aggregator/src/main/java/java/ru/practicum/AggregatorApp.java
@@ -0,0 +1,15 @@
+package java.ru.practicum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import java.ru.practicum.service.AggregationStarter;
+
+@SpringBootApplication
+public class AggregatorApp {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(AggregatorApp.class, args);
+        AggregationStarter aggregator = context.getBean(AggregationStarter.class);
+        aggregator.start();
+    }
+}
