1415df0a808eb1a1d4ec8a96680d29fa9c70d421
Maksim Torzhkov <maxtorj@yandex.ru>
2026-03-03 18:43:00 +0300

    refactoring preparation

--- core/interaction-api/src/main/java/ru/practicum/exception/TooManyRequestsException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.exception;
-
-public class TooManyRequestsException extends RuntimeException {
-    public TooManyRequestsException(String message) {
-        super(message);
-    }
-}
--- core/interaction-api/src/main/java/ru/practicum/exception/UpdateStartDateException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.exception;
-
-public class UpdateStartDateException extends RuntimeException {
-    public UpdateStartDateException(String message) {
-        super(message);
-    }
-}
