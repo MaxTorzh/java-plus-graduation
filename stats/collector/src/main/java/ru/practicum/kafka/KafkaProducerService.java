d071ceea1407de82d9304e73da912e1c1dcad00f
Maksim Torzhkov <maxtorj@yandex.ru>
2026-03-03 08:36:18 +0300

    feat: kafka producer

--- stats/collector/src/main/java/ru/practicum/kafka/KafkaProducerService.java
+++ stats/collector/src/main/java/ru/practicum/kafka/KafkaProducerService.java
@@ -2,6 +2,7 @@ package ru.practicum.kafka;
 
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.avro.specific.SpecificRecordBase;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
@@ -15,20 +16,33 @@ import java.time.Duration;
 import java.time.Instant;
 import java.util.Properties;
 
+@Slf4j
 @Service
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class KafkaProducerService implements AutoCloseable {
-    final KafkaProducer<Long, SpecificRecordBase> producer;
+    KafkaProducer<Long, SpecificRecordBase> producer;
+    boolean enabled = true;
 
-    public KafkaProducerService(@Value("${kafka.bootstrap-servers}") String bootstrapServers) {
-        Properties config = new Properties();
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());
-        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, AvroSerializer.class.getName());
-        this.producer = new KafkaProducer<>(config);
+    public KafkaProducerService(
+            @Value("${kafka.bootstrap-servers:}") String bootstrapServers,
+            @Value("${kafka.enabled:true}") boolean enabled) {
+
+        this.enabled = enabled && !bootstrapServers.isEmpty();
+
+        if (this.enabled) {
+            Properties config = new Properties();
+            config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+            config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());
+            config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, AvroSerializer.class.getName());
+            this.producer = new KafkaProducer<>(config);
+        }
     }
 
     public void send(SpecificRecordBase action, Long eventId, Instant timestamp, String topic) {
+        if (!enabled) {
+            log.warn("Kafka producer disabled, message not sent: {}", action);
+            return;
+        }
         ProducerRecord<Long, SpecificRecordBase> record = new ProducerRecord<>(
                 topic,
                 null,
@@ -42,7 +56,9 @@ public class KafkaProducerService implements AutoCloseable {
 
     @Override
     public void close() {
+        if (enabled && producer != null) {
             producer.flush();
             producer.close(Duration.ofSeconds(5));
+        }
     }
 }
